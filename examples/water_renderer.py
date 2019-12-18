cc5a8f305d2a7235198bfd651c15c50a38b42272
Yuanming Hu <yuanmhu@gmail.com>
2019-12-18 17:19:24 -0500

    update examples

--- examples/water_renderer.py
+++ examples/water_renderer.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import cv2
 
 real = ti.f32
 ti.set_default_fp(real)
@@ -31,12 +32,10 @@ height_gradient = vec()
 bottom_image = scalar()
 refracted_image = scalar()
 
-# mode = 'reflect'
 mode = 'refract'
 
-assert mode in ['reflect', 'refract', 'photon']
-
 # ti.cfg.arch = ti.cuda
+
 
 @ti.layout
 def place():
@@ -62,15 +61,14 @@ learning_rate = 0.1
 # TODO: there may by out-of-bound accesses here
 @ti.func
 def laplacian(t, i, j):
-  return inv_dx2 * (
-      -4 * p[t, i, j] + p[t, i, j - 1] + p[t, i, j + 1] + p[t, i + 1, j] +
-      p[t, i - 1, j])
+  return inv_dx2 * (-4 * p[t, i, j] + p[t, i, j - 1] + p[t, i, j + 1] +
+                    p[t, i + 1, j] + p[t, i - 1, j])
 
 
 @ti.func
 def gradient(t, i, j):
   return 0.5 * inv_dx * ti.Vector(
-    [p[t, i + 1, j] - p[t, i - 1, j], p[t, i, j + 1] - p[t, i, j - 1]])
+      [p[t, i + 1, j] - p[t, i - 1, j], p[t, i, j + 1] - p[t, i, j - 1]])
 
 
 @ti.kernel
@@ -87,8 +85,8 @@ def fdtd(t: ti.i32):
       laplacian_p = laplacian(t - 2, i, j)
       laplacian_q = laplacian(t - 1, i, j)
       p[t, i, j] = 2 * p[t - 1, i, j] + (
-          c * c * dt * dt + c * alpha * dt) * laplacian_q - p[
-                     t - 2, i, j] - c * alpha * dt * laplacian_p
+          c * c * dt * dt + c * alpha * dt
+      ) * laplacian_q - p[t - 2, i, j] - c * alpha * dt * laplacian_p
 
 
 @ti.kernel
@@ -102,16 +100,16 @@ def render_reflect(t: ti.i32):
 
 @ti.func
 def pattern(i, j):
-  return ti.cast(ti.floor(i / (n_grid / 8)) + ti.floor(j / (n_grid / 8)),
-                 ti.i32) % 2
+  return ti.cast(
+      ti.floor(i / (n_grid / 8)) + ti.floor(j / (n_grid / 8)), ti.i32) % 2
 
 
 @ti.kernel
-def render_refract(t: ti.i32):
+def render_refract():
   for i in range(n_grid):  # Parallelized over GPU threads
     for j in range(n_grid):
       grad = height_gradient[i, j]
-      
+
       scale = 2.0
       sample_x = i - grad[0] * scale
       sample_y = j - grad[1] * scale
@@ -119,20 +117,16 @@ def render_refract(t: ti.i32):
       sample_y = ti.min(n_grid - 1, ti.max(0, sample_y))
       sample_xi = ti.cast(ti.floor(sample_x), ti.i32)
       sample_yi = ti.cast(ti.floor(sample_y), ti.i32)
-      
+
       frac_x = sample_x - sample_xi
       frac_y = sample_y - sample_yi
-      
+
       for k in ti.static(range(3)):
         refracted_image[i, j, k] = (1.0 - frac_x) * (
-            (1 - frac_y) * bottom_image[sample_xi, sample_yi, k] + frac_y *
-            bottom_image[
-              sample_xi, sample_yi + 1, k]) + frac_x * (
-                                       (1 - frac_y) * bottom_image[
-                                     sample_xi + 1, sample_yi, k] + frac_y *
-                                       bottom_image[
-                                         sample_xi + 1, sample_yi + 1, k]
-                                   )
+            (1 - frac_y) * bottom_image[sample_xi, sample_yi, k] +
+            frac_y * bottom_image[sample_xi, sample_yi + 1, k]) + frac_x * (
+                (1 - frac_y) * bottom_image[sample_xi + 1, sample_yi, k] +
+                frac_y * bottom_image[sample_xi + 1, sample_yi + 1, k])
 
 
 @ti.kernel
@@ -141,43 +135,6 @@ def compute_height_gradient(t: ti.i32):
     for j in range(n_grid):
       # TODO: fix boundary
       height_gradient[i, j] = gradient(t, i, j)
-
-
-@ti.kernel
-def clear_photon_map():
-  for i in range(n_grid):
-    for j in range(n_grid):
-      rendered[i, j] = 0.0
-
-
-@ti.kernel
-def render_photon_map(t: ti.i32, offset_x: ti.f32, offset_y: ti.f32):
-  for i in range(n_grid):  # Parallelized over GPU threads
-    for j in range(n_grid):
-      grad = height_gradient[i, j] * (1 - offset_x) * (1 - offset_y) + \
-             height_gradient[i + 1, j] * offset_x * (1 - offset_y) + \
-             height_gradient[i, j + 1] * (1 - offset_x) * offset_y + \
-             height_gradient[i + 1, j + 1] * offset_x * offset_y
-      
-      scale = 5.0
-      sample_x = i - grad[0] * scale + offset_x
-      sample_y = j - grad[1] * scale + offset_y
-      sample_x = ti.min(n_grid - 1, ti.max(0, sample_x))
-      sample_y = ti.min(n_grid - 1, ti.max(0, sample_y))
-      sample_xi = ti.cast(ti.floor(sample_x), ti.i32)
-      sample_yi = ti.cast(ti.floor(sample_y), ti.i32)
-      
-      frac_x = sample_x - sample_xi
-      frac_y = sample_y - sample_yi
-      
-      x = sample_xi
-      y = sample_yi
-      
-      ti.atomic_add(rendered[x, y], (1 - frac_x) * (1 - frac_y))
-      ti.atomic_add(rendered[x, y + 1], (1 - frac_x) * frac_y)
-      ti.atomic_add(rendered[x + 1, y], frac_x * (1 - frac_y))
-      ti.atomic_add(rendered[x + 1, y + 1], frac_x * frac_y)
-
 
 @ti.kernel
 def compute_loss(t: ti.i32):
@@ -194,26 +151,16 @@ def apply_grad():
 
 
 def forward(output=None):
-  steps_mul = 1
   interval = vis_interval
   if output:
     os.makedirs(output, exist_ok=True)
     interval = output_vis_interval
   initialize()
-  for t in range(2, steps * steps_mul):
+  for t in range(2, steps):
     fdtd(t)
-    if (t + 1) % interval == 0:
-      clear_photon_map()
-      compute_height_gradient()
-      if mode == 'refract':
-        render_refract()
-      elif mode == 'photon':
-        render_photon_map(t, 0.25, 0.25)
-        render_photon_map(t, 0.25, 0.75)
-        render_photon_map(t, 0.75, 0.25)
-        render_photon_map(t, 0.75, 0.75)
-      else:
-        render_reflect()
+    if (t + 1) % interval == 0 and output is not None:
+      compute_height_gradient(t)
+      render_refract()
       if mode == 'refract':
         img = np.zeros(shape=(n_grid, n_grid, 3), dtype=np.float32)
         for i in range(n_grid):
@@ -237,8 +184,8 @@ def forward(output=None):
         if output:
           img = np.clip(img, 0, 255)
           cv2.imwrite(output + "/{:04d}.png".format(t), img * 255)
-  loss[None] = 0
-  compute_loss(steps - 1)
+  compute_height_gradient(steps - 1)
+  render_refract()
 
 
 def main():
@@ -248,44 +195,35 @@ def main():
     for j in range(256):
       for k in range(3):
         bottom_image[i, j, k] = bot_img[i, j, k]
-  target_img = cv2.imread('iclr2020.png')[:, :, 0] / 255.0
-  target_img = cv2.resize(target_img, (n_grid, n_grid))
-  target_img -= target_img.mean()
-  cv2.imshow('target', target_img * amplify + 0.5)
-  # print(target_img.min(), target_img.max())
-  for i in range(n_grid):
-    for j in range(n_grid):
-      target[i, j] = float(target_img[i, j])
-  
+
   initial[n_grid // 2, n_grid // 2] = 1
   # forward('water_renderer/initial')
   initial[n_grid // 2, n_grid // 2] = 0
-  
+
   from adversarial import vgg_grad, predict
-  
+
   for opt in range(10):
     with ti.Tape(loss):
       forward()
-      
+
       feed_to_vgg = np.zeros((224, 224, 3), dtype=np.float32)
       # Note: do a transpose here
       for i in range(224):
         for j in range(224):
           for k in range(3):
-            feed_to_vgg[i, j, k] = refracted_image[i + 16, j + 16, 2-k]
-      
+            feed_to_vgg[i, j, k] = refracted_image[i + 16, j + 16, 2 - k]
+
       predict(feed_to_vgg)
       grad = vgg_grad(feed_to_vgg)
       for i in range(224):
         for j in range(224):
           for k in range(3):
-            refracted_image.grad[i + 16, j + 16, k] = grad[i, j, 2-k] * 0.001
-      
-    
+            refracted_image.grad[i + 16, j + 16, k] = grad[i, j, 2 - k] * 0.001
+
     print('Iter', opt, ' Loss =', loss[None])
-    
+
     apply_grad()
-  
+
   forward('water_renderer/optimized')
 
 
