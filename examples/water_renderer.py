7ac1003ce4d48b4d2483714feab33b824500a751
Mingrui <mingrui.zhang@taichi.graphics>
2021-10-11 01:46:45 +0800

    upgrade to taichi v0.8.1

--- examples/water_renderer.py
+++ examples/water_renderer.py
@@ -125,7 +125,7 @@ def compute_height_gradient(t: ti.i32):
 def compute_loss(t: ti.i32):
     for i in range(n_grid):
         for j in range(n_grid):
-            ti.atomic_add(loss, dx * dx * (target[i, j] - p[t, i, j]))**2
+            ti.atomic_add(loss[None], dx * dx * (target[i, j] - p[t, i, j]))**2
 
 
 @ti.kernel
